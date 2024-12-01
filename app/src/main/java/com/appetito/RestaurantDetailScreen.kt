400a818cd1b15d69d2225ff9314e9b121c1d8d84
l4mole <danielrojo9822@gmail.com>
2024-11-30 23:14:44 -0600

    Modificacion en RestaurantDetailScreen.kt: Se le agrego el boton de Search, y modificacion al color del  bnt Return

--- app/src/main/java/com/appetito/RestaurantDetailScreen.kt
+++ app/src/main/java/com/appetito/RestaurantDetailScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,7 +29,17 @@ fun RestaurantDetailScreen(navController: NavHostController ,restaurantName: Str
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Regresar"
+                            contentDescription = "Regresar",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* Lógica para el botón de búsqueda */ }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Buscar",
+                            tint = MaterialTheme.colorScheme.onPrimary
                         )
                     }
                 },
